30131351bff402b112c02b30005500225175f065
Nishant Bansal <bansalnishant3516@gmail.com>
2022-06-07 17:18:56 +0530

    First commit

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    content: ["./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+        letterSpacing: {
+            tightest: '-.075em',
+            tighter: '-.05em',
+            tight: '-.025em',
+            normal: '0',
+            wide: '.025em',
+            wider: '.05em',
+            widest: '0.4em',
+        },
+        fontFamily: {
+            'sans': ['ui-sans-serif', 'system-ui'],
+            'serif': ['ui-serif', 'Georgia'],
+            'mono': ['ui-monospace', 'SFMono-Regular'],
+            'display': ['Oswald'],
+            'body': ['"Open Sans"'],
+            'task': ['Josefin Sans']
+        },
+        fontWeight: {
+            hairline: 100,
+            'extra-light': 100,
+            thin: 200,
+            light: 300,
+            normal: 400,
+            medium: 500,
+            semibold: 600,
+            bold: 700,
+            extrabold: 800,
+            'extra-bold': 800,
+            black: 900,
+        }
+    },
+    plugins: [],
+}
